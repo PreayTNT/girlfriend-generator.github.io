5a1ee8bb8fa301f2db6b190a81783b41e041419d
PreayTNT <84791379+PreayTNT@users.noreply.github.com>
2022-03-21 22:31:06 +0530

    main

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const app = ExpressXD();
 app.get("/", (req, res) => {
   res.sendFile("index.html", {root: __dirname})
 })
+app.get("/iasked", (rew, res => {
+  res.redirect("https://preaytnt.glitch.me");
+}))
